72dcf359418249e315f7137f7cc190c0f61f7e5b
nathan <000savior001@gmail.com>
2024-02-18 23:33:28 -0800

    connected to atlas, userSchema defined, user model listening on route /auth/signup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 
 const indexRouter = require('./routes/index');
@@ -8,13 +9,23 @@ const authRouter = require('./routes/auth');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
+console.log(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+})
+.then(() => {
+    console.log("MongoDB connection is estabilised!!");
+})
+.catch(err => console.error("mongoDB connection error: ", err))
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
-app.use("/auth", authRouter);
+app.use("/auth", authRouter); 
 
 app.listen(PORT, function(){
     console.log(`Listening on port ${PORT}`);
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,47 @@
 var express = require("express");
 var router = express.Router();
+const { hash } = require("bcryptjs");
 
-module.export = router;
+const User = require("../models/user")
+
+router.post("./signup", async(req, res) =>{
+    try{
+        const { email, password } = req.body;
+        //1. check if user already exists
+        const user = await User.findOne({ email:email });
+        // if user exists
+        if(user)
+            return res.status(500).json({
+                message: "User already exists! Try logging in.",
+                type: "warning",
+            });
+        
+        //2. user does not exists, create a new user
+        const passwordHash = await hash(password, 10);
+        const newUser = new User({
+            email:email,
+            password:passwordHash,
+        });
+        //3. save the user to the database
+        await newUser.save();
+        //4. send the response
+        res.status(200).json({
+            message:"user created successfully!",
+            type: "success",
+        });
+        
+    } catch(error){
+        res.status(500).json({
+            type: "error",
+            message: "Error creating user!", error,
+        });
+    }
+});
+
+// const user1 = new userSchema({})
+router.get('/', (req, res) =>{
+    res.send("hello from the auth router");
+});
+
+
+module.exports = router;
